72dfa7ca6c5ea04af9ad2ab4306d5f3ed13e854f
Kevin Kim <kebonkim@gmail.com>
2024-01-31 12:35:24 -0800

    Updated Login with icon and validation

--- App.js
+++ App.js
@@ -3,37 +3,48 @@ import { useState } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { createStackNavigator } from "@react-navigation/stack";
 
 import LoginPage from './src/screens/LoginPage';
 
 import background from './assets/background.png'
 import NavigationBar from './src/components/NavigationBar';
+import SignUpPage from './src/screens/SignUpPage';
 
 export default function App() {
+  
+  const [fakeLogin, setFakeLogin] = useState(false);
 
-  const [isLoggedIn, setLoggedIn] = useState(false);
 
-  // temporary sign in
+  const Stack = createStackNavigator();
+
   const fakeSignIn = (data) => {
-    setLoggedIn(data)
+    setFakeLogin(data)
   }
 
   return (
     <GestureHandlerRootView style={styles.container}>
-        <SafeAreaProvider>
-          <NavigationContainer>
-            {isLoggedIn ? (
-              <NavigationBar></NavigationBar>
-            ) : (
-              <ImageBackground
-                source={background}
-                style={styles.view_container}
+      <SafeAreaProvider>
+        <NavigationContainer>
+          {fakeLogin ? (
+            <NavigationBar></NavigationBar>
+          ) : (
+            <ImageBackground source={background} style={styles.view_container}>
+              <Stack.Navigator
+                screenOptions={{
+                  headerShown: false,
+                }}
               >
-                <LoginPage setLoggedIn={fakeSignIn} />
-              </ImageBackground>
-            )}
-          </NavigationContainer>
-        </SafeAreaProvider>
+                <Stack.Screen name="LoginPage">
+                  {(props) => <LoginPage {...props} setFakeLogin={setFakeLogin}/>}
+                </Stack.Screen>
+                
+                {/* <Stack.Screen name="SignUpPage" component={SignUpPage} /> */}
+              </Stack.Navigator>
+            </ImageBackground>
+          )}
+        </NavigationContainer>
+      </SafeAreaProvider>
     </GestureHandlerRootView>
   );
 }
--- src/screens/LoginPage.js
+++ src/screens/LoginPage.js
@@ -1,82 +1,183 @@
-import {View, Text, StyleSheet, TextInput, TouchableOpacity, Pressable, Image} from 'react-native';
+import {View, Text, StyleSheet, TextInput, TouchableOpacity, Pressable, Image, ImageBackground, TouchableWithoutFeedback, Keyboard, KeyboardAvoidingView, Platform} from 'react-native';
 import { useCustomFonts } from "../utils/CustomFonts";
 import { Ionicons } from "@expo/vector-icons";
+import background from '../../assets/background.png'
+
+import passicon from '../../assets/icons/misc/passicon.png'
+import emailicon from '../../assets/icons/misc/emailicon.png'
+
+
+import { useState, useEffect, useRef } from 'react';
 
 
 
-import { useState } from 'react';
+function LoginPage ({navigation, setFakeLogin}) {
 
+      const secondInputRef = useRef(null);
 
-function LoginPage ({setLoggedIn}) {
     const fontsLoaded = useCustomFonts();
     const [isChecked, setChecked] = useState(false);
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [errors, setErrors] = useState([])
+    const [submitted, setSubmitted] = useState(false)
+
+
+    // useEffect(() => {
+    //   let errorMessage = []
+    //   if (!email.includes('@')) {
+    //     errorMessage.push('Invalid email')
+    //   }
+
+    //   if (!password) {
+    //     errorMessage.push('Please enter your password')
+    //   }
+
+    //   setErrors(errorMessage)
+
+    // }, [email, password])
+
 
     if (!fontsLoaded) {
       return null;
     }
 
+
     // temporary sign in
     const signIn = () => {
-      if (email.length > 0 && password.length > 0) {
-        setLoggedIn(true)
-      }
-    }
+  let errorMessage = [];
+
+  if (!email.includes('@')) {
+    errorMessage.push('Invalid email');
+  }
+
+  if (!password) {
+    errorMessage.push('Please enter your password');
+  }
+
+  setErrors(errorMessage);
+
+  if (email.length > 0 && password.length > 0 && errorMessage.length === 0) {
+    setFakeLogin(true);
+  }
+
+  setSubmitted(!submitted);
+};
 
     return (
       <View style={styles.l_container}>
-          <View style={styles.l_box}>
-        <View style={{alignItems: 'center'}}>
-        <Image 
-            source={require('../../assets/Logo.png')}
-        />
-        </View>
-        <View style={{height: 350, justifyContent:'space-between'}}>
-            <Text style={styles.l_text_top}>Login</Text>
-            <Text style={styles.l_text_bot}>Please sign in to continue</Text>
-            <TextInput
-              style={styles.l_input}
-              placeholder="email"
-              onChangeText={(e) => setEmail(e)}
-              value={email}
-            />
-            <TextInput
-              style={styles.l_input}
-              placeholder="password"
-              onChangeText={(p) => setPassword(p)}
-              secureTextEntry
-              value={password}
-            />
-            <View style={styles.l_checkbox}>
-              <Pressable
-                style={[styles.checkbox, isChecked && styles.l_checked]}
-                onPress={() => setChecked(!isChecked)}
-              >
-                {isChecked && (
-                  <Ionicons name="checkmark" size={18} color="white" />
-                )}
-              </Pressable>
-              <Text style={styles.l_rememberme}>
-                Remember me
-              </Text>
-            </View>
-            <TouchableOpacity style={styles.l_button} onPress={() => signIn()} disabled={!email && !password}>
-              <View>
-                <Text style={{ color: "white" }}>Sign in</Text>
+        <KeyboardAvoidingView
+          behavior={Platform.OS === "ios" ? "padding" : "height"}
+          style={styles.l_keyboardAvoidingView}
+        >
+          <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <ImageBackground style={styles.l_background} source={background}>
+              <View style={styles.l_box}>
+                <View style={{ alignItems: "center" }}>
+                  <Image source={require("../../assets/Logo.png")} />
+                </View>
+                <View style={{ height: 350, justifyContent: "space-between" }}>
+                  <Text style={styles.l_text_top}>Login</Text>
+                  <Text style={styles.l_text_bot}>
+                    Please sign in to continue
+                  </Text>
+                  <View>
+                    <TextInput
+                      style={styles.l_input}
+                      placeholder="email"
+                      onChangeText={(e) => setEmail(e)}
+                      value={email}
+                      keyboardType="email-address"
+                      returnKeyType="next"
+                      onSubmitEditing={() => {
+                        secondInputRef.current.focus();
+                      }}
+                    />
+                    <Image style={styles.l_input_icon_e} source={emailicon} />
+                    <View style={{ height: 20, marginLeft: 15 }}>
+                      {errors.length && submitted ? (
+                        <Text style={styles.errortext}>{"Invalid Email"}</Text>
+                      ) : (
+                        <Text></Text>
+                      )}
+                    </View>
+                  </View>
+                  <View>
+                    <TextInput
+                      ref={secondInputRef}
+                      style={styles.l_input}
+                      placeholder={"password"}
+                      onChangeText={(p) => setPassword(p)}
+                      secureTextEntry
+                      value={password}
+                      returnKeyType="done"
+                      onSubmitEditing={() => {
+                        signIn();
+                        Keyboard.dismiss();
+                      }}
+                    />
+                    <Image style={styles.l_input_icon_p} source={passicon} />
+                    <View style={{ height: 20, marginLeft: 15 }}>
+                      {errors.length && submitted ? (
+                        <Text style={styles.errortext}>
+                          {"Please enter your password"}
+                        </Text>
+                      ) : (
+                        <Text></Text>
+                      )}
+                    </View>
+                  </View>
+                  <View style={styles.l_checkbox}>
+                    <Pressable
+                      style={[styles.checkbox, isChecked && styles.l_checked]}
+                      onPress={() => setChecked(!isChecked)}
+                    >
+                      {isChecked && (
+                        <Ionicons name="checkmark" size={18} color="white" />
+                      )}
+                    </Pressable>
+                    {/* <TouchableOpacity
+                      onPress={() => navigation.navigate("SignUpPage")}
+                    >
+                      <Text>Sign up</Text>
+                    </TouchableOpacity> */}
+                    <Text style={styles.l_rememberme}>Remember me</Text>
+                  </View>
+                  <TouchableOpacity
+                    style={styles.l_button}
+                    onPress={() => signIn()}
+                  >
+                    <View>
+                      <Text style={{ color: "white" }}>Sign in</Text>
+                    </View>
+                  </TouchableOpacity>
+                </View>
               </View>
-            </TouchableOpacity>
-        </View>
-          </View>
+            </ImageBackground>
+          </TouchableWithoutFeedback>
+        </KeyboardAvoidingView>
       </View>
     );
 };
 
 const styles = StyleSheet.create({
+  l_keyboardAvoidingView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+  },
   l_container: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  l_background: {
+    flex: 1,
+    resizeMode: "cover",
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "center",
   },
   l_box: {
     justifyContent: "space-around",
@@ -96,11 +197,22 @@ const styles = StyleSheet.create({
     height: 45,
     borderRadius: 25,
     marginBottom: 5,
+    paddingLeft: 45,
     padding: 15,
     shadowColor: "black",
     shadowOffset: { width: -1, height: 2 },
     shadowOpacity: 0.2,
     shadowRadius: 5,
+  },
+  l_input_icon_e: {
+    position: "absolute",
+    top: 15,
+    left: 18,
+  },
+  l_input_icon_p: {
+    position: "absolute",
+    top: 13,
+    left: 22,
   },
   l_checkbox: {
     fontFamily: "Lato-Reg",
@@ -139,6 +251,11 @@ const styles = StyleSheet.create({
   l_rememberme: {
     color: "green",
     fontSize: 16,
+  },
+  errortext: {
+    fontFamily: "Lato-Bold",
+    fontSize: 12,
+    color: "red",
   },
 });
 
